d859537b463d79f217d54b684ada1191a8dfc3f9
Đặng Nhật Minh <2251172420@e.tlu.edu.vn>
2025-12-25 10:04:44 +0700

    update book search

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'firebase_options.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const TramDocApp());
+  print('[Firestore] projectId=${FirebaseFirestore.instance.app.options.projectId}');
 }
